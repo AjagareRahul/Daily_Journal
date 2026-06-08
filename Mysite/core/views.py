7fb996e78ec2ecda673760f7f7b316b626032b1c
Ajagare Rahul <ajagarerahul@gmail.com>
2026-06-08 17:05:23 +0530

    Use PostgreSQL; add Student model & templates
    Switch project DATABASES to PostgreSQL (mydb on localhost:5432) and clear project-level template DIRS. Add a Student model (name, age, city) with __str__ and add migrations (0001_initial, 0002_student_city). Update views to pass student data to templates (home uses a sample list; about queries Student objects). Modify templates (home, about, contact) to iterate and display students and fix template block endings. Updated compiled bytecode (__pycache__) accompanies the changes.

--- Mysite/core/views.py
+++ Mysite/core/views.py
@@ -1,15 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Student
 
 # Create your views here.
 
 def home(request):
-    return render(request,'home.html')
+    student = [
+    {'name': "Rahul", 'age': 22, 'city': 'Latur'}
+]
+
+    return render(request,'home.html',{'students':student})
    # return HttpResponse("Django Started")
 
 def about(request):
-    return render(request,'about.html')
+    students=Student.objects.filter(age=22)
+    
+    #student=Student.objects.all()
+    return render(request,'about.html',{'students':students})
     #return HttpResponse("About Page")
+    '''student=[
+        {'name':'Rahul','sirname':'Ajagare','age':22},
+        {'name':'Danny','sirname':'Ajagare','age':23},
+    ]
+    '''
 
 def contact(request):
     return render(request,'contact.html')
