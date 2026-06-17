814f1a01bb8274d92880a10de155713ad485f20e
Ajagare Rahul <ajagarerahul@gmail.com>
2026-06-17 16:15:08 +0530

    Add pagination and update templates
    Rename template blocks and restructure home/about templates; add search/sort form, student table with proper looping and empty-state, and pagination controls. Update views.py to use Django Paginator for 3 items per page, import Q (search code left commented) and return a sample student in about view. Remove an unused base block and update template block name from 'containt' to 'content'.

--- StudentHub/Hub/views.py
+++ StudentHub/Hub/views.py
@@ -1,17 +1,43 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from Hub.models import Student
+from django.db.models import Q
+from django.core.paginator import Paginator
 
 def home(request):
-    students=Student.objects.all()
-    return render(request,'home.html',{'students':students})
+
+    students = Student.objects.all()
+
+    # pagination
+    paginator = Paginator(students, 3)   # 3 per page
+    page_number = request.GET.get('page')
+    students = paginator.get_page(page_number)
+
+    return render(request, 'home.html', {'students': students})
+'''
+    query = request.GET.get('q')
+    if query:
+        students = students.filter(
+            Q(fname__icontains=query) | Q(college__icontains=query)
+        )
+
+    sort = request.GET.get('sort')
+    if sort == "asc":
+        students = students.order_by('age')
+    elif sort == "desc":
+        students = students.order_by('-age')
+
+    return render(request, 'home.html', {'students': students})'''
    
    
    
    # return HttpResponse("This is home page")
  
 def about(request):
-    return render(request,'about.html')
+    student=Student.objects.filter(age=22).first()
+    return render(request,'about.html',{'student':student})
+
+
     #return HttpResponse("This is about page")
 
 
